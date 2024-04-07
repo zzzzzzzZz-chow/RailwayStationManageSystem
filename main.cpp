228cc1040e3bfd22b683a2259c94b3d1e6ab4149
zzzzzzzzZz-chow <1416271215@qq.com>
2024-04-07 18:29:48 +0800

    更换数据集*2

--- main.cpp
+++ main.cpp
@@ -4,9 +4,12 @@
 
 int main() {
   using namespace station_system;
-  fstream fs{"test.data", ios::in | ios::out};
+  fstream fs{"test.data", ios::in};
   auto t = make_shared<BaseTrainManage>();
   t->load(fs);
   t->save(cout);
+  fs.close();
+  fs.open("test.data", ios::out);
   t->save(fs);
+  fs.close();
 }
