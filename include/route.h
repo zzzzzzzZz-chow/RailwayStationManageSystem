cd4426c090137d0a0dbfce4d548a7c20c6442bd8
zzzzzzzzZz-chow <1416271215@qq.com>
2024-04-07 16:38:28 +0800

    stations底层实现变更

--- include/route.h
+++ include/route.h
@@ -1,12 +1,11 @@
 #pragma once
 
 #include "../include/station.h"
-#include <initializer_list>
 #include <vector>
 
 namespace station_system {
 
-using Stations = initializer_list<Station>;
+using Stations = vector<Station>;
 using StationIterator = vector<Station>::const_iterator;
 class BaseRoute;
 using Route = shared_ptr<BaseRoute>;
